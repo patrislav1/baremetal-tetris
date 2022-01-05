f52f1c554aa0be23fce6365b6ae098621c220bf6
Patrick Huesmann <info@patrick-huesmann.de>
2022-01-05 14:25:50 +0100

    Add FP test

--- app/app.c
+++ app/app.c
@@ -8,29 +8,36 @@
 #include "gpio.h"
 #include "uart.h"
 
-static coop_task_t mytask1, mytask2;
-static uint8_t mystack1[2000];
-static uint8_t mystack2[2000];
-
-static void test_task_1(void* arg)
+static void led_test_fn(void* arg)
 {
     size_t l = 100;
     while (l--) {
         ((l & 1) ? LL_GPIO_SetOutputPin : LL_GPIO_ResetOutputPin)(LD4_GPIO_Port, LD4_Pin);
+        // delay_ms() will implicitly call sched_yield()
         delay_ms(100);
     }
-    printf("Task1 Watermark: %u\r\n", get_stack_watermark(&mytask1));
 }
 
-static void test_task_2(void* arg)
+static void fp_test_fn(void* arg)
 {
-    size_t l = 150;
-    while (l--) {
-        printf("Countdown %d\r\n", l);
+    static float f = 0.f;
+    while (f < 10.f) {
+        printf("Value: %g\r\n", f);
+        f += 0.1f;
+        // delay_ms() will implicitly call sched_yield()
         delay_ms(110);
     }
-    printf("Task2 Watermark: %u\r\n", get_stack_watermark(&mytask2));
 }
+
+static struct {
+    const char* name;
+    coop_task_fn_t task_fn;
+    coop_task_t task;
+    uint8_t stack[1000];
+} tasks[] = {
+    {"LED", led_test_fn},
+    {"FP", fp_test_fn},
+};
 
 void app_main(void)
 {
@@ -38,13 +45,26 @@ void app_main(void)
     delay_init();
 
     sched_init();
-    sched_create_task(&mytask1, test_task_1, NULL, mystack1, sizeof(mystack1));
-    sched_create_task(&mytask2, test_task_2, NULL, mystack2, sizeof(mystack2));
+
+    for (size_t k = 0; k < sizeof(tasks) / sizeof(tasks[0]); k++) {
+        sched_create_task(&tasks[k].task,
+                          tasks[k].task_fn,
+                          NULL,
+                          tasks[k].stack,
+                          sizeof(tasks[k].stack));
+    }
 
     while (1) {
         char c;
+        // read() will implicitly call sched_yield()
         if (read(STDIN_FILENO, &c, sizeof(c)) == sizeof(c)) {
             printf("received: %c\r\n", c);
+        }
+        if (c == 'w') {
+            for (size_t k = 0; k < sizeof(tasks) / sizeof(tasks[0]); k++) {
+                size_t wm = get_stack_watermark(&tasks[k].task);
+                printf("Stack watermark of %s task: %u\r\n", tasks[k].name, wm);
+            }
         }
     }
 }
